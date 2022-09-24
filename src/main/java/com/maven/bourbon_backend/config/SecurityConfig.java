e6ecc740386148549f335a2ce0ba17faaa5dfa31
Ted Kruse <tpk5348@psu.edu>
2022-09-24 01:26:43 -0400

    Updated CORS settings with relation to options

--- src/main/java/com/maven/bourbon_backend/config/SecurityConfig.java
+++ src/main/java/com/maven/bourbon_backend/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.maven.bourbon_backend.filter.CustomAuthorizationFilter;
 import com.maven.bourbon_backend.service.RefreshService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,8 +39,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean(), refreshService);
+        customAuthenticationFilter.setFilterProcessesUrl("/BourbonCommunityReviews/profile/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().antMatchers("/BourbonCommunityReviews/profile/login").permitAll();
         http.authorizeRequests().antMatchers("/BourbonCommunityReviews/profile/refresh").permitAll();
         http.authorizeRequests().antMatchers("/BourbonCommunityReviews/profile/logout").permitAll();
         http.authorizeRequests().antMatchers("/BourbonCommunityReviews/profile/new").permitAll();
@@ -48,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(GET, "/BourbonCommunityReviews/bourbon").hasAnyAuthority("Admin");
         http.authorizeRequests().anyRequest().authenticated();
         //http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean(), refreshService));
+        http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
