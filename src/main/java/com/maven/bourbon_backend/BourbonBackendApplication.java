671a4c0213d0b59173e9a2371bbb6cc5d0243c53
Ted Kruse <tpk5348@psu.edu>
2022-11-05 15:52:16 -0400

    Updated CORS filter for deployed frontend

--- src/main/java/com/maven/bourbon_backend/BourbonBackendApplication.java
+++ src/main/java/com/maven/bourbon_backend/BourbonBackendApplication.java
@@ -33,7 +33,7 @@ public class BourbonBackendApplication {
 	public CorsFilter corsFilter(){
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","https://bourboncommunityreviews-front.herokuapp.com/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("application/json", "Origin","ContentType", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept", "X-Requested_With", "Access-Control-Request-Method", "Access-Control-Request-Headers", "If-Match", "If-None-Match"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("application/json", "Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
